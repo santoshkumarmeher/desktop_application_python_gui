b194a1111a3d3c1757bfbc1e7ec2f9a60b9ea202
SANTOSH KUMAR MEHER <santoshkumarmeher13@gmail.com>
2023-12-02 23:04:00 +0530

    Improved UI

--- 23_feedback_window.py
+++ 23_feedback_window.py
@@ -1,50 +1,67 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import messagebox
 import pandas as pd
 from openpyxl import load_workbook
+import customtkinter
 
-def save_feedback():
-    full_name = full_name_entry.get()
-    email_id = email_entry.get()
-    age = age_entry.get()
-    feedback = feedback_var.get()
 
-    data = {'Full Name': [full_name], 'Email ID': [email_id], 'Age': [age], 'Feedback': [feedback]}
-    df = pd.DataFrame(data)
+def center_window(app, width, height):
+    screen_width = app.winfo_screenwidth()
+    screen_height = app.winfo_screenheight()
+    x_position = (screen_width - width) // 2
+    y_position = (screen_height - height) // 2
 
-    try:
-        existing_df = pd.read_excel('feedback_data.xlsx', sheet_name='FeedbackData')
-        updated_df = pd.concat([existing_df, df], ignore_index=True)
-        updated_df.to_excel('feedback_data.xlsx', index=False, sheet_name='FeedbackData', engine='openpyxl')
-    except FileNotFoundError:
+    app.geometry(f"{width}x{height}+{x_position}+{y_position}")
 
-        df.to_excel('feedback_data.xlsx', index=False, sheet_name='FeedbackData', engine='openpyxl')
 
-    full_name_entry.delete(0, tk.END)
-    email_entry.delete(0, tk.END)
-    age_entry.delete(0, tk.END)
+def create_boxes():
+    app = customtkinter.CTk()
+    app.title("Feedback Form")
+    def save_feedback():
+        if full_name_entry.get() == "":
+            messagebox.showwarning("Incomplete Form", "Name field is mandatory. Please fill it.")
+        else:
+            full_name = full_name_entry.get()
+            email_id = email_entry.get()
+            age = age_entry.get()
+            feedback = feedback_dropdown.get()
+            data = {'Full Name': [full_name], 'Email ID': [email_id], 'Age': [age], 'Feedback': [feedback]}
+            df = pd.DataFrame(data)
 
-root = tk.Tk()
-root.title("Feedback Form")
+            try:
+                existing_df = pd.read_excel('feedback_data.xlsx', sheet_name='FeedbackData')
+                updated_df = pd.concat([existing_df, df], ignore_index=True)
+                updated_df.to_excel('feedback_data.xlsx', index=False, sheet_name='FeedbackData', engine='openpyxl')
+                
+            except FileNotFoundError:
+                df.to_excel('feedback_data.xlsx', index=False, sheet_name='FeedbackData', engine='openpyxl')
+            messagebox.showinfo("Submitted", "Thank you for your valuable feedback !")
+        full_name_entry.delete(0, tk.END)
+        email_entry.delete(0, tk.END)
+        age_entry.delete(0, tk.END)
 
-tk.Label(root, text="Full Name:").grid(row=0, column=0, sticky=tk.W, padx=10, pady=5)
-tk.Label(root, text="Email ID:").grid(row=1, column=0, sticky=tk.W, padx=10, pady=5)
-tk.Label(root, text="Age:").grid(row=2, column=0, sticky=tk.W, padx=10, pady=5)
-tk.Label(root, text="Feedback:").grid(row=3, column=0, sticky=tk.W, padx=10, pady=5)
 
-full_name_entry = tk.Entry(root)
-full_name_entry.grid(row=0, column=1, padx=10, pady=5)
-email_entry = tk.Entry(root)
-email_entry.grid(row=1, column=1, padx=10, pady=5)
-age_entry = tk.Entry(root)
-age_entry.grid(row=2, column=1, padx=10, pady=5)
+    customtkinter.CTkLabel(app, text="We Value Your Feedback",font=("",20)).grid(row=0, column=1, columnspan=3, sticky=tk.W, padx=70,pady=30)
+    customtkinter.CTkLabel(app, text="Full Name:").grid(row=1, column=0, sticky=tk.W, padx=50, pady=5)
+    customtkinter.CTkLabel(app, text="Email ID:").grid(row=2, column=0, sticky=tk.W, padx=50, pady=5)
+    customtkinter.CTkLabel(app, text="Age:").grid(row=3, column=0, sticky=tk.W, padx=50, pady=5)
+    customtkinter.CTkLabel(app, text="Feedback:").grid(row=3, column=2, sticky=tk.W, padx=10, pady=5)
 
-feedback_var = tk.StringVar()
-feedback_options = ["Excellent", "Good", "Average", "Poor"]
-feedback_dropdown = ttk.Combobox(root, textvariable=feedback_var, values=feedback_options)
-feedback_dropdown.grid(row=3, column=1, padx=10, pady=5)
+    full_name_entry = customtkinter.CTkEntry(app,width=350)
+    full_name_entry.grid(row=1, column=1, padx=10, pady=5,columnspan=3)
+    email_entry = customtkinter.CTkEntry(app,width=350)
+    email_entry.grid(row=2, column=1, padx=10, pady=5,columnspan=3)
+    age_entry = customtkinter.CTkEntry(app,width=90)
+    age_entry.grid(row=3, column=1, padx=10, pady=5)
+    
+    feedback_options = ["Excellent", "Good", "Average", "Poor"]
+    feedback_dropdown = customtkinter.CTkComboBox(app,values=feedback_options,width=150)
+    feedback_dropdown.grid(row=3, column=3, padx=10, pady=5)
 
-save_button = tk.Button(root, text="Save Feedback", command=save_feedback)
-save_button.grid(row=4, column=0, columnspan=2, pady=10)
+    save_button = customtkinter.CTkButton(app, text="Submit", command=save_feedback)
+    save_button.grid(row=4, column=3,pady=10)
 
-root.mainloop()
+
+    center_window(app, 600, 300)
+    app.mainloop()
+create_boxes()
